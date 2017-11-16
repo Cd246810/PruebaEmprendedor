e84384eeb1d1a437df484f20d00c57e128637f49
Cd246810 <christian246810@gmail.com>
2017-11-16 12:38:14 -0600

    PR

--- index.php
+++ index.php
@@ -212,9 +212,9 @@
 
 <!-- Container (The Band Section) -->
 <div id="Somos" class="container text-center">
-  <h3>THE BAND</h3>
-  <p><em>We love music!</em></p>
-  <p>We have created a fictional band website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  <h3>El Equipo</h3>
+  <p><em>Un grupo de estudiantes emprendedores</em></p>
+  <p>Con el apoyo del curso de emprendimiento de negocios informaticos hemos realizado el staturp Seats. El cual ayudara a mejorar la calidad de vida de los guatemaltecos al momento de usar el transporte publico.</p>
   <br>
   <div class="row">
     <div class="col-sm-4">
