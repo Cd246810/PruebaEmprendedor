0db07ca154f1e56e5039859923971735a8c3a5f0
Cd246810 <christian246810@gmail.com>
2017-11-17 02:14:26 -0600

    Im

--- index.php
+++ index.php
@@ -247,35 +247,20 @@
     <div class="col-sm-4">
       <p class="text-center"><strong>Christian Escobar</strong></p><br>
       <a href="#demo" data-toggle="collapse">
-        <img src="Pases.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+        <img src="C.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
       </a>
-      <div id="demo" class="collapse">
-        <p>Guitarist and Lead Vocalist</p>
-        <p>Loves long walks on the beach</p>
-        <p>Member since 1988</p>
-      </div>
     </div>
     <div class="col-sm-4">
       <p class="text-center"><strong>Kevin Juarez</strong></p><br>
       <a href="#demo2" data-toggle="collapse">
-        <img src="Pases.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+        <img src="K.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
       </a>
-      <div id="demo2" class="collapse">
-        <p>Drummer</p>
-        <p>Loves drummin'</p>
-        <p>Member since 1988</p>
-      </div>
     </div>
     <div class="col-sm-4">
       <p class="text-center"><strong>Abner Cojon</strong></p><br>
       <a href="#demo3" data-toggle="collapse">
-        <img src="Pases.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
+        <img src="A.jpg" class="img-circle person" alt="Random Name" width="255" height="255">
       </a>
-      <div id="demo3" class="collapse">
-        <p>Bass player</p>
-        <p>Loves math</p>
-        <p>Member since 2005</p>
-      </div>
     </div>
   </div>
 </div>
